09d53a85da65038f11d03d747c140776d3d78852
Yurii Savchuk <geo.savchuk@gmail.com>
2021-01-02 22:29:34 +0200

    Added dynamic routing.

--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,29 @@
-import { createRouter, createWebHistory } from "vue-router";
-import EventList from "../views/EventList.vue";
-import About from '@/views/About.vue';
+import { createRouter, createWebHistory } from "vue-router"
+import EventList from "../views/EventList.vue"
+import About from '@/views/About.vue'
+import EventDetails from "@/views/EventDetails";
 const routes = [
   {
-    path: "/",
-    name: "EventList",
+    path: '/',
+    name: 'EventList',
     component: EventList
   },
   {
-    path: "/about",
-    name: "About",
+    path: '/event/:id',
+    name: 'EventDetails',
+    props: true,
+    component: EventDetails
+  },
+  {
+    path: '/about',
+    name: 'About',
     component: About
   }
-];
+]
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes
-});
+})
 
-export default router;
+export default router
